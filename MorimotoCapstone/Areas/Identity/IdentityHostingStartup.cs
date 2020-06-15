45d821bc97c65f2831f513501f38d275e2547cc1
Steve Morimoto <smorimoto2284@gmail.com>
2020-06-15 16:08:00 -0500

    initial

---
+++ MorimotoCapstone/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MorimotoCapstone.Data;
+
+[assembly: HostingStartup(typeof(MorimotoCapstone.Areas.Identity.IdentityHostingStartup))]
+namespace MorimotoCapstone.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ MorimotoCapstone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MorimotoCapstone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
